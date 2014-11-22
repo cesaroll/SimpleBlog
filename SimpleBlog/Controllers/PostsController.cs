4792edf51d2d924ab92b6bb9a5cbfbfcf4a2d8a5
unknown <locesar@LOCESAR2.americas.hpqcorp.net>
2014-11-22 16:35:56 -0700

    Adding controllers and Admin area

---
+++ SimpleBlog/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        /// <summary>
+        /// Index Action
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return Content("Hello, world!");
+        }
+    }
+}
