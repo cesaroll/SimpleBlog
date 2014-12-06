4ea7ae9833d784aa1237d880b3ce9d10757f9e02
unknown <locesar@LOCESAR2.americas.hpqcorp.net>
2014-12-05 18:02:17 -0700

    Created new and edit actions for the posts controller

--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.UI.WebControls;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
 using SimpleBlog.Infrastructure;
 using SimpleBlog.Models;
 
@@ -7,5 +8,18 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
