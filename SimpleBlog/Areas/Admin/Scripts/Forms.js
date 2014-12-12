0c6484045da89dbcca4544a3e92d300195c2f0df
unknown <locesar@LOCESAR2.americas.hpqcorp.net>
2014-12-11 21:56:10 -0700

    Delete, restore and trash posts Slugify component

--- SimpleBlog/Areas/Admin/Scripts/Forms.js
+++ SimpleBlog/Areas/Admin/Scripts/Forms.js
@@ -18,6 +18,24 @@
             .append(antiForgeryInput)
             .appendTo(document.body)
             .submit();
-    })
+    });
+
+    $("[data-slug]").each(function() {
+        var $this = $(this);
+        var $sendSlugFrom = $($this.data("slug"));
+
+        $sendSlugFrom.keyup(function() {
+            var slug = $sendSlugFrom.val();
+            slug = slug.replace(/[^a-zA-Z0-9\s]/g,"");
+            slug = slug.toLowerCase().trim();
+            slug = slug.replace(/\s+/g, "-");
+
+            if (slug.charAt(slug.length - 1) == "-")
+                slug = slug.substr(0, slug.length - 1);
+
+            $this.val(slug);
+
+        });
+    });
 
 })
